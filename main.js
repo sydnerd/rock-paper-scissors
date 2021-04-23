22a71c83f1fc61db960a7a393974126b8beb9a26
Sydney Bear <sbear3915@gmail.com>
2021-04-23 17:08:46 -0600

    Create a function to allow emoji to appear when rock is selected

--- main.js
+++ main.js
@@ -4,10 +4,13 @@ var difficultGameButton = document.getElementById("difficultGameButton");
 var classicFighters = document.getElementById("classicFighters");
 var difficultFighters = document.getElementById("difficultFighters")
 var gameHeading = document.getElementById("gameHeading");
+var rockImage = document.getElementById("rockImage");
+var rockIcon = document.getElementById("rockIcon");
 
 // EVENT LISTENERS
 classicGameButton.addEventListener("click", classicView)
 difficultGameButton.addEventListener("click", difficultView)
+classicFighters.addEventListener("click", selectFighter)
 
 
 
@@ -28,6 +31,18 @@ function gameView(){
   hide(difficultGameButton, false);
   hide(classicGameButton, false);
   gameHeading.innerText= "Choose your fighter!"
+}
+
+function selectFighter(){
+  if(event.target.id === "rockImage"){
+    hide(rockIcon, true)
+  }
+  if(event.target.id === "paperImage"){
+    hide(rockIcon, true)
+  }
+  if(event.target.id === "scissorsImage"){
+    hide(rockIcon, true)
+  }
 }
 
 function hide(element, hidden) {
