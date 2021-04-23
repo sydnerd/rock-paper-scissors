9d6eb5ed2d0770049a26697cc99819fbc46ed0b2
Sydney Bear <sbear3915@gmail.com>
2021-04-23 15:19:15 -0600

    Update margin

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 class Game {
   constructor(player1, player2, gameType){
     this.player1 = player1;
-    this.player2 =player2;
+    this.player2 = player2;
     this.board = "";
     this.gameType = gameType;
     this.turn = false;
