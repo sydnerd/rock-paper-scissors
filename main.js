2ec30943d0862fe53d034c5fa81ec664fe56bc7d
Sydney Bear <sbear3915@gmail.com>
2021-04-23 16:51:08 -0600

    Show classic fighter and difficult figher options for the difficult game view

--- main.js
+++ main.js
@@ -2,19 +2,32 @@
 var classicGameButton = document.getElementById("classicGameButton");
 var difficultGameButton = document.getElementById("difficultGameButton");
 var classicFighters = document.getElementById("classicFighters");
-var
+var difficultFighters = document.getElementById("difficultFighters")
+var gameHeading = document.getElementById("gameHeading");
 
 // EVENT LISTENERS
 classicGameButton.addEventListener("click", classicView)
+difficultGameButton.addEventListener("click", difficultView)
 
 
 
 
  // EVENT HANDLERS AND GLOBAL FUNCTIONS
 function classicView(){
+  gameView();
+  hide(classicFighters, true);
+}
+
+function difficultView(){
+  gameView();
+  hide(classicFighters, true);
+  hide(difficultFighters,true);
+}
+
+function gameView(){
   hide(difficultGameButton, false);
   hide(classicGameButton, false);
-  hide(classicFighters, true);
+  gameHeading.innerText= "Choose your fighter!"
 }
 
 function hide(element, hidden) {
