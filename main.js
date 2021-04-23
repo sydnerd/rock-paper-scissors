7b3f3cc462fcdd608d05202fe21c227caa11e239
Sydney Bear <sbear3915@gmail.com>
2021-04-23 16:35:11 -0600

    Show 3 classic fighter options when classic button is clicked

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 // VARIABLES FOR HTML ELEMENTS
 var classicGameButton = document.getElementById("classicGameButton");
 var difficultGameButton = document.getElementById("difficultGameButton");
+var classicFighters = document.getElementById("classicFighters");
+var
 
 // EVENT LISTENERS
 classicGameButton.addEventListener("click", classicView)
@@ -10,8 +12,9 @@ classicGameButton.addEventListener("click", classicView)
 
  // EVENT HANDLERS AND GLOBAL FUNCTIONS
 function classicView(){
-  hide(difficultGameButton, false)
-  hide(classicGameButton, false)
+  hide(difficultGameButton, false);
+  hide(classicGameButton, false);
+  hide(classicFighters, true);
 }
 
 function hide(element, hidden) {
